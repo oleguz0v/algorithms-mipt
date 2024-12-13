c9e2b5a2f324a193949250bc113eff4cdaa37df1
oleguz0v <podguzov.on@phystech.edu>
2024-12-13 17:10:25 +0300

    Update contest2_taskI.cpp

--- contest2_taskI.cpp
+++ contest2_taskI.cpp
@@ -1,3 +1,10 @@
+/*
+ Вам нужно реализовать структуру данных, которая поддерживает очередь элементов и обрабатывает запросы следующих типов:
+ + x: добавить число x в конец очереди;
+ * x: вставить число x в середину очереди (если в очереди нечётное число элементов, то x встаёт сразу за центральным элементом);
+ -: удалить первый элемент очереди и вывести его значение.
+ */
+
 #include <algorithm>
 #include <deque>
 #include <iostream>
@@ -8,31 +15,37 @@ class MidDeque {
   std::deque<int> first_;
   std::deque<int> second_;
 
-  void Balance() {
-    if (second_.size() > first_.size()) {
-      first_.push_back(second_.front());
-      second_.pop_front();
-    }
-  }
+  void Balance();
 
  public:
-  void PushBack(int value) {
-    second_.push_back(value);
-    Balance();
-  }
-
-  void PushMiddle(int value) {
-    second_.push_front(value);
-    Balance();
-  }
-
-  int PopFront() {
-    int res = first_.front();
-    first_.pop_front();
-    Balance();
-    return res;
-  }
+  void PushBack(int value);
+  void PushMiddle(int value);
+  int PopFront();
 };
+
+void MidDeque::Balance() {
+  if (second_.size() > first_.size()) {
+    first_.push_back(second_.front());
+    second_.pop_front();
+  }
+}
+
+void MidDeque::PushBack(int value) {
+  second_.push_back(value);
+  Balance();
+}
+
+void MidDeque::PushMiddle(int value) {
+  second_.push_front(value);
+  Balance();
+}
+
+int MidDeque::PopFront() {
+  int res = first_.front();
+  first_.pop_front();
+  Balance();
+  return res;
+}
 
 int main() {
   int n;
