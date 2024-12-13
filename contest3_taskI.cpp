9f9b9299e72911e6ba7371cd6e52d8b2fdc09a03
oleguz0v <podguzov.on@phystech.edu>
2024-12-13 18:28:55 +0300

    Update contest3_taskI.cpp

--- contest3_taskI.cpp
+++ contest3_taskI.cpp
@@ -22,72 +22,80 @@ class SegmentTree {
     Build(0, 0, n_);
   }
 
-  size_t CountInRange(int q_l, int q_r, int x) {
-    return Query(0, 0, n_, q_l, q_r, x);
-  }
+  size_t CountInRange(int q_l, int q_r, int x);
 
  private:
   int n_;
   vector<int> a_;
   vector<vector<int>> tree_;
 
-  void Build(int i, int l, int r) {
-    tree_[i].push_back(0);
-    if (l + 1 == r) {
-      tree_[i].push_back(a_[l]);
-    } else {
-      int m = (l + r) / 2;
-      Build(2 * i + 1, l, m);
-      Build(2 * i + 2, m, r);
-      MergeNodes(tree_[2 * i + 1], tree_[2 * i + 2], tree_[i]);
-    }
-  }
-
+  void Build(int i, int l, int r);
   void static MergeNodes(vector<int>& left, vector<int>& right,
-                         vector<int>& parent) {
-    size_t p1 = 0;
-    size_t p2 = 0;
-    while (p1 < left.size() and p2 < right.size()) {
-      if (left[p1] < right[p2]) {
-        parent.push_back(left[p1++]);
-      } else {
-        parent.push_back(right[p2++]);
-      }
-    }
-    while (p1 < left.size()) {
+                         vector<int>& parent);
+  size_t Query(int i, int l, int r, int q_l, int q_r, int x);
+  size_t static CountLessOrEqual(vector<int>& node, int x);
+};
+
+void SegmentTree::Build(int i, int l, int r) {
+  tree_[i].push_back(0);
+  if (l + 1 == r) {
+    tree_[i].push_back(a_[l]);
+  } else {
+    int m = (l + r) / 2;
+    Build(2 * i + 1, l, m);
+    Build(2 * i + 2, m, r);
+    MergeNodes(tree_[2 * i + 1], tree_[2 * i + 2], tree_[i]);
+  }
+}
+
+size_t SegmentTree::CountInRange(int q_l, int q_r, int x) {
+  return Query(0, 0, n_, q_l, q_r, x);
+}
+
+void SegmentTree::MergeNodes(vector<int>& left, vector<int>& right,
+                       vector<int>& parent) {
+  size_t p1 = 0;
+  size_t p2 = 0;
+  while (p1 < left.size() and p2 < right.size()) {
+    if (left[p1] < right[p2]) {
       parent.push_back(left[p1++]);
-    }
-    while (p2 < right.size()) {
+    } else {
       parent.push_back(right[p2++]);
     }
   }
-
-  size_t Query(int i, int l, int r, int q_l, int q_r, int x) {
-    if (q_r <= l || r <= q_l) {
-      return 0;
-    }
-    if (q_l <= l && r <= q_r) {
-      return CountLessOrEqual(tree_[i], x);
-    }
-    int m = (l + r) / 2;
-    return Query(2 * i + 1, l, m, q_l, q_r, x) +
-           Query(2 * i + 2, m, r, q_l, q_r, x);
+  while (p1 < left.size()) {
+    parent.push_back(left[p1++]);
   }
-
-  size_t static CountLessOrEqual(vector<int>& node, int x) {
-    size_t l_p = 0;
-    size_t r_p = node.size();
-    while (r_p - l_p > 1) {
-      size_t buf = (l_p + r_p) / 2;
-      if (node[buf] <= x) {
-        l_p = buf;
-      } else {
-        r_p = buf;
-      }
-    }
-    return l_p + 1;
+  while (p2 < right.size()) {
+    parent.push_back(right[p2++]);
   }
-};
+}
+
+size_t SegmentTree::Query(int i, int l, int r, int q_l, int q_r, int x) {
+  if (q_r <= l || r <= q_l) {
+    return 0;
+  }
+  if (q_l <= l && r <= q_r) {
+    return CountLessOrEqual(tree_[i], x);
+  }
+  int m = (l + r) / 2;
+  return Query(2 * i + 1, l, m, q_l, q_r, x) +
+         Query(2 * i + 2, m, r, q_l, q_r, x);
+}
+
+size_t SegmentTree::CountLessOrEqual(vector<int>& node, int x) {
+  size_t l_p = 0;
+  size_t r_p = node.size();
+  while (r_p - l_p > 1) {
+    size_t buf = (l_p + r_p) / 2;
+    if (node[buf] <= x) {
+      l_p = buf;
+    } else {
+      r_p = buf;
+    }
+  }
+  return l_p + 1;
+}
 
 int main() {
   int n;
