810929f56af7c270190fc5ec2376dad4f60f1aae
oleguz0v <podguzov.on@phystech.edu>
2024-12-13 18:00:53 +0300

    Update contest3_taskA.cpp

--- contest3_taskA.cpp
+++ contest3_taskA.cpp
@@ -1,3 +1,7 @@
+/*
+Реализовать структура данных для нахождения второго максимума на отрезке (усложненные спарсы)
+*/
+
 #include <algorithm>
 #include <cmath>
 #include <iostream>
@@ -10,51 +14,57 @@ const int cInf = 1e9;
 
 class SparseTable {
  public:
-  SparseTable(const std::vector<int>& array) {
-    n_ = (int)array.size();
-    a_ = array;
-    PrecomputeLog();
-    BuildTable();
-  }
-
-  int Query(int l, int r) const {
-    int d = flog2_[r - l];
-    if (m_[d][l][0] < m_[d][r - (1 << d)][0]) {
-      return m_[d][l][1];
-    }
-    return m_[d][r - (1 << d)][1];
-  }
+  SparseTable(const std::vector<int>& array);
+  int Query(int l, int r) const;
 
  private:
   int n_;
   std::vector<int> a_;
   std::vector<std::vector<std::vector<int>>> m_;
   std::vector<int> flog2_;
+  
+  void PrecomputeLog();
+  void BuildTable();
+};
 
-  void PrecomputeLog() {
-    flog2_.resize(n_ + 1);
-    for (int i = 2; i <= n_; ++i) {
-      flog2_[i] = flog2_[i / 2] + 1;
-    }
+SparseTable::SparseTable(const std::vector<int>& array) {
+  n_ = (int)array.size();
+  a_ = array;
+  PrecomputeLog();
+  BuildTable();
+}
+
+int SparseTable::Query(int l, int r) const {
+  int d = flog2_[r - l];
+  if (m_[d][l][0] < m_[d][r - (1 << d)][0]) {
+    return m_[d][l][1];
   }
+  return m_[d][r - (1 << d)][1];
+}
 
-  void BuildTable() {
-    m_.resize(flog2_[n_] + 1, std::vector<std::vector<int>>(n_));
-    for (int i = 0; i < n_; ++i) {
-      m_[0][i].push_back(a_[i]);
-      m_[0][i].push_back(i);
-    }
-    for (int d = 1; d <= flog2_[n_]; ++d) {
-      for (int i = 0; i + (1 << d) <= n_; ++i) {
-        if (m_[d - 1][i][0] < m_[d - 1][i + (1 << (d - 1))][0]) {
-          m_[d][i] = m_[d - 1][i];
-        } else {
-          m_[d][i] = m_[d - 1][i + (1 << (d - 1))];
-        }
+void SparseTable::PrecomputeLog() {
+  flog2_.resize(n_ + 1);
+  for (int i = 2; i <= n_; ++i) {
+    flog2_[i] = flog2_[i / 2] + 1;
+  }
+}
+
+void SparseTable::BuildTable() {
+  m_.resize(flog2_[n_] + 1, std::vector<std::vector<int>>(n_));
+  for (int i = 0; i < n_; ++i) {
+    m_[0][i].push_back(a_[i]);
+    m_[0][i].push_back(i);
+  }
+  for (int d = 1; d <= flog2_[n_]; ++d) {
+    for (int i = 0; i + (1 << d) <= n_; ++i) {
+      if (m_[d - 1][i][0] < m_[d - 1][i + (1 << (d - 1))][0]) {
+        m_[d][i] = m_[d - 1][i];
+      } else {
+        m_[d][i] = m_[d - 1][i + (1 << (d - 1))];
       }
     }
   }
-};
+}
 
 int FindSecondMinimum(const SparseTable& st, const std::vector<int>& a, int l,
                       int r) {
