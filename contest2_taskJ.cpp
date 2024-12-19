2110c831a4fa4f5e89f246b4933e5ba03f258c24
oleguz0v <podguzov.on@phystech.edu>
2024-12-20 00:54:57 +0300

    Add files via upload

--- contest2_taskJ.cpp
+++ contest2_taskJ.cpp
@@ -1,63 +1,68 @@
-#include <algorithm>
-#include <cmath>
+/*
+В этой задаче вам предстоит реализовать очередь с поддержкой минимума.
+*/
+
 #include <deque>
 #include <iostream>
-#include <map>
-#include <set>
-#include <string>
-#include <vector>
 
 class QueueWithMin {
- private:
-  std::deque<int> queue_;
-  std::deque<int> minQueue_;
-
- public:
-  void Front() const {
-    if (queue_.empty()) {
-      std::cout << "error" << '\n';
-    } else {
-      std::cout << queue_.front() << '\n';
-    }
-  }
-
-  void Size() const { std::cout << queue_.size() << '\n'; }
-
-  void GetMin() const {
-    if (minQueue_.empty()) {
-      std::cout << "error" << '\n';
-    } else {
-      std::cout << minQueue_.front() << '\n';
-    }
-  }
-
-  void Clear() {
-    queue_.clear();
-    minQueue_.clear();
-    std::cout << "ok" << '\n';
-  }
-
-  void Dequeue() {
-    if (queue_.empty()) {
-      std::cout << "error" << '\n';
-    } else {
-      std::cout << queue_.front() << '\n';
-      if (!minQueue_.empty() and minQueue_.front() == queue_.front()) {
-        minQueue_.pop_front();
-      }
-      queue_.pop_front();
-    }
-  }
-
-  void Enqueue(int value) {
-    queue_.push_back(value);
-    while (!minQueue_.empty() and minQueue_.back() > value) {
-      minQueue_.pop_back();
-    }
-    minQueue_.push_back(value);
-    std::cout << "ok" << '\n';
-  }
+  public:
+    void Front() const;
+    void Size() const;
+    void GetMin() const;
+    void Clear();
+    void Dequeue();
+    void Enqueue(int value);
+    
+  private:
+    std::deque<int> queue_;
+    std::deque<int> minQueue_;
 };
+
+void QueueWithMin::Front() const {
+  if (queue_.empty()) {
+    std::cout << "error" << '\n';
+  } else {
+    std::cout << queue_.front() << '\n';
+  }
+}
+
+void QueueWithMin::Size() const { std::cout << queue_.size() << '\n'; }
+
+void QueueWithMin::GetMin() const {
+  if (minQueue_.empty()) {
+    std::cout << "error" << '\n';
+  } else {
+    std::cout << minQueue_.front() << '\n';
+  }
+}
+
+void QueueWithMin::Clear() {
+  queue_.clear();
+  minQueue_.clear();
+  std::cout << "ok" << '\n';
+}
+
+void QueueWithMin::Dequeue() {
+  if (queue_.empty()) {
+    std::cout << "error" << '\n';
+  } else {
+    std::cout << queue_.front() << '\n';
+    if (!minQueue_.empty() and minQueue_.front() == queue_.front()) {
+      minQueue_.pop_front();
+    }
+    queue_.pop_front();
+  }
+}
+
+void QueueWithMin::Enqueue(int value) {
+  queue_.push_back(value);
+  while (!minQueue_.empty() and minQueue_.back() > value) {
+    minQueue_.pop_back();
+  }
+  minQueue_.push_back(value);
+  std::cout << "ok" << '\n';
+}
 
 int main() {
   int q;
