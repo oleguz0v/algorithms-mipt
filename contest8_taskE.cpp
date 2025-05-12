6237aa4756400eb4ac68b86aceeb56c0a9d103c4
oleguz0v <podguzov.on@phystech.edu>
2025-05-13 01:26:32 +0300

    Update contest8_taskE.cpp

--- contest8_taskE.cpp
+++ contest8_taskE.cpp
@@ -1,92 +1,123 @@
 // Нахождение отрицательного цикла с помощью алгоритма Форда-Беллмана
 
-#include <algorithm>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using std::cin;
 using std::cout;
 using std::endl;
-using std::max;
-using std::min;
 using std::vector;
 
 const int cNoEdge = 100000;
 
-class NegativeCycleFinder {
+class Graph {
  public:
-  NegativeCycleFinder(int n) {
-    n_ = n;
-    a_.resize(n_ + 1, vector<long long>(n_ + 1));
-    d_.resize(n_ + 1);
-    par_.resize(n_ + 1);
+  explicit Graph(int vertex_count) {
+    vertex_count_ = vertex_count;
+    adj_matrix_.resize(vertex_count_ + 1, vector<long long>(vertex_count_ + 1));
   }
 
-  void ReadGraph() {
-    for (int i = 1; i <= n_; ++i) {
-      for (int j = 1; j <= n_; ++j) {
-        cin >> a_[i][j];
-      }
-    }
+  void SetEdgeWeight(int from, int to, long long weight) {
+    adj_matrix_[from][to] = weight;
   }
 
-  void FindCycle() {
-    for (int i = 1; i <= n_; ++i) {
-      d_[i] = 0;
-      par_[i] = -1;
+  bool HasNegativeCycle(vector<int>& cycle_out) {
+    InitializeDistances();
+    int cycle_start = RelaxEdges();
+    if (cycle_start == -1) {
+      return false;
     }
-    int x = -1;
-    for (int it = 1; it <= n_; ++it) {
-      x = -1;
-      for (int u = 1; u <= n_; ++u) {
-        for (int v = 1; v <= n_; ++v) {
-          if (a_[u][v] != cNoEdge) {
-            if (d_[v] > d_[u] + a_[u][v]) {
-              d_[v] = d_[u] + a_[u][v];
-              par_[v] = u;
-              x = v;
+    cycle_start = FindCycleStart(cycle_start);
+    cycle_out = RecoverCycle(cycle_start);
+    return true;
+  }
+
+ private:
+  int vertex_count_;
+  vector<vector<long long>> adj_matrix_;
+  vector<long long> distances_;
+  vector<int> parent_;
+
+  void InitializeDistances() {
+    distances_.assign(vertex_count_ + 1, 0);
+    parent_.assign(vertex_count_ + 1, -1);
+  }
+
+  int RelaxEdges() {
+    int last_updated = -1;
+    for (int iter = 1; iter <= vertex_count_; ++iter) {
+      last_updated = -1;
+      for (int u = 1; u <= vertex_count_; ++u) {
+        for (int v = 1; v <= vertex_count_; ++v) {
+          if (adj_matrix_[u][v] != cNoEdge) {
+            if (distances_[v] > distances_[u] + adj_matrix_[u][v]) {
+              distances_[v] = distances_[u] + adj_matrix_[u][v];
+              parent_[v] = u;
+              last_updated = v;
             }
           }
         }
       }
     }
-    if (x == -1) {
-      cout << "NO" << endl;
-      return;
+    return last_updated;
+  }
+
+  int FindCycleStart(int start) const {
+    for (int i = 1; i <= vertex_count_; ++i) {
+      start = parent_[start];
     }
-    for (int i = 1; i <= n_; ++i) {
-      x = par_[x];
-    }
-    int now = x;
+    return start;
+  }
+
+  vector<int> RecoverCycle(int start) const {
+    vector<int> cycle;
+    int current = start;
     while (true) {
-      cycle_.push_back(now);
-      now = par_[now];
-      if (now == x and (int) cycle_.size() > 1) {
+      cycle.push_back(current);
+      current = parent_[current];
+      if (current == start && cycle.size() > 1) {
         break;
       }
     }
-    cycle_.push_back(x);
-    std::reverse(cycle_.begin(), cycle_.end());
+    cycle.push_back(start);
+    std::reverse(cycle.begin(), cycle.end());
+    return cycle;
+  }
+};
+
+void ReadGraph(Graph& graph, int n) {
+  for (int i = 1; i <= n; ++i) {
+    for (int j = 1; j <= n; ++j) {
+      long long weight;
+      cin >> weight;
+      graph.SetEdgeWeight(i, j, weight);
+    }
+  }
+}
+
+void PrintCycleResult(const vector<int>& cycle) {
+  if (cycle.empty()) {
+    cout << "NO" << endl;
+  } else {
     cout << "YES" << endl;
-    cout << (int)cycle_.size() << endl;
-    for (int i = 0; i < (int)cycle_.size(); ++i) {
-      cout << cycle_[i] << ' ';
+    cout << (int)cycle.size() << endl;
+    for (int v : cycle) {
+      cout << v << ' ';
     }
     cout << endl;
   }
-
- private:
-  int n_;
-  vector<vector<long long>> a_;
-  vector<long long> d_;
-  vector<int> par_;
-  vector<int> cycle_;
-};
+}
 
 int main() {
   int n;
   cin >> n;
-  NegativeCycleFinder solver(n);
-  solver.ReadGraph();
-  solver.FindCycle();
+  Graph graph(n);
+  ReadGraph(graph, n);
+  vector<int> cycle;
+  if (graph.HasNegativeCycle(cycle)) {
+    PrintCycleResult(cycle);
+  } else {
+    PrintCycleResult({});
+  }
 }
