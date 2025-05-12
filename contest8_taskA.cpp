4848afa5c9d2966510eb98abe708beadb38487ae
oleguz0v <podguzov.on@phystech.edu>
2025-05-13 01:11:54 +0300

    Update contest8_taskA.cpp

--- contest8_taskA.cpp
+++ contest8_taskA.cpp
@@ -11,78 +11,75 @@ using std::pair;
 using std::priority_queue;
 using std::vector;
 
-const long long cInf = 2009000999;
+const int cInf = 2009000999;
 
-class DijkstraSolver {
+class Graph {
  public:
-  DijkstraSolver(int n, int m) {
-    n_ = n;
-    m_ = m;
-    gr_.resize(n_);
-    d_.assign(n_, cInf);
+  explicit Graph(int vertex_count) {
+    vertex_count_ = vertex_count;
+    adjacency_list_.resize(vertex_count_);
+    distances_.assign(vertex_count_, cInf);
   }
 
-  void AddEdge(int u, int v, int c) {
-    gr_[u].push_back({v, c});
-    gr_[v].push_back({u, c});
+  void AddEdge(int from, int to, int cost) {
+    adjacency_list_[from].push_back({to, cost});
+    adjacency_list_[to].push_back({from, cost});
   }
 
-  void Run(int s) {
-    d_.assign(n_, cInf);
-    d_[s] = 0;
-    priority_queue<pair<int, int>> q;
-    q.push({0, s});
-    while (!q.empty()) {
-      int now = q.top().second;
-      int dist = -q.top().first;
-      q.pop();
-      if (d_[now] < dist) {
+  void RunDijkstra(int start_vertex) {
+    distances_.assign(vertex_count_, cInf);
+    distances_[start_vertex] = 0;
+    priority_queue<pair<int, int>> pq;
+    pq.push({0, start_vertex});
+    while (!pq.empty()) {
+      int current = pq.top().second;
+      int dist = -pq.top().first;
+      pq.pop();
+      if (distances_[current] < dist) {
         continue;
       }
-      for (auto el : gr_[now]) {
-        if (d_[el.first] > d_[now] + el.second) {
-          d_[el.first] = d_[now] + el.second;
-          q.push({-d_[el.first], el.first});
+      for (auto [neighbor, weight] : adjacency_list_[current]) {
+        if (distances_[neighbor] > distances_[current] + weight) {
+          distances_[neighbor] = distances_[current] + weight;
+          pq.push({-distances_[neighbor], neighbor});
         }
       }
     }
   }
 
   void PrintDistances() const {
-    for (int i = 0; i < n_; ++i) {
-      cout << d_[i] << ' ';
+    for (int i = 0; i < vertex_count_; ++i) {
+      cout << distances_[i] << ' ';
     }
     cout << endl;
   }
 
  private:
-  int n_;
-  int m_;
-  vector<vector<pair<int, int>>> gr_;
-  vector<int> d_;
+  int vertex_count_;
+  vector<vector<pair<int, int>>> adjacency_list_;
+  vector<int> distances_;
 };
 
-int main() {
-  int t;
-  cin >> t;
-  while ((t--) > 0) {
-    int n;
-    int m;
-    cin >> n;
-    cin >> m;
-    DijkstraSolver solver(n, m);
-    for (int i = 0; i < m; ++i) {
+void SolveDijkstraTests() {
+  int test_count;
+  cin >> test_count;
+  while (test_count-- > 0) {
+    int vertex_count;
+    int edge_count;
+    cin >> vertex_count >> edge_count;
+    Graph graph(vertex_count);
+    for (int i = 0; i < edge_count; ++i) {
       int u;
       int v;
-      int c;
-      cin >> u;
-      cin >> v;
-      cin >> c;
-      solver.AddEdge(u, v, c);
+      int w;
+      cin >> u >> v >> w;
+      graph.AddEdge(u, v, w);
     }
-    int s;
-    cin >> s;
-    solver.Run(s);
-    solver.PrintDistances();
+    int start_vertex;
+    cin >> start_vertex;
+    graph.RunDijkstra(start_vertex);
+    graph.PrintDistances();
   }
 }
+
+int main() { SolveDijkstraTests(); }
