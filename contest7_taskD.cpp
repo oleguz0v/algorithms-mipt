8442275a8c6495a51e4d7ccc81d7e887f378c460
oleguz0v <podguzov.on@phystech.edu>
2025-05-26 21:51:35 +0300

    Update contest7_taskD.cpp
    https://contest.yandex.ru/contest/75531/run-report/138810162/

--- contest7_taskD.cpp
+++ contest7_taskD.cpp
@@ -17,11 +17,15 @@ class Graph {
     vertex_count_ = vertex_count;
     adjacency_list_.resize(vertex_count_ + 1);
   }
+
   void AddEdge(int from, int to) { adjacency_list_[from].push_back(to); }
+
   int VertexCount() const { return vertex_count_; }
+
   const vector<vector<int>>& GetAdjacencyList() const {
     return adjacency_list_;
   }
+
   Graph Transpose() const {
     Graph transposed(vertex_count_);
     for (int from = 1; from <= vertex_count_; ++from) {
@@ -31,25 +35,24 @@ class Graph {
     }
     return transposed;
   }
-  friend int CalculateStrongConnectedComponents(const Graph& graph,
-                                                vector<int>& component);
 
- private:
-  int vertex_count_;
-  vector<vector<int>> adjacency_list_;
-  void DFS(int v, vector<bool>& visited, vector<int>& result,
-           int current_component, vector<int>* component) const {
+  void DepthFirstSearch(int v, vector<bool>& visited, vector<int>& result,
+                        int current_component, vector<int>* component) const {
     visited[v] = true;
     if (component != nullptr) {
       (*component)[v] = current_component;
     }
     for (int u : adjacency_list_[v]) {
       if (!visited[u]) {
-        DFS(u, visited, result, current_component, component);
+        DepthFirstSearch(u, visited, result, current_component, component);
       }
     }
     result.push_back(v);
   }
+
+ private:
+  int vertex_count_;
+  vector<vector<int>> adjacency_list_;
 };
 
 // Основная функция Косарайю
@@ -58,23 +61,33 @@ int CalculateStrongConnectedComponents(const Graph& graph,
   int n = graph.VertexCount();
   vector<bool> visited(n + 1, false);
   vector<int> order;
+
+  // Заполнение order с использованием поиска в глубину (DFS)
   for (int i = 1; i <= n; ++i) {
     if (!visited[i]) {
-      graph.DFS(i, visited, order, -1, nullptr);
+      graph.DepthFirstSearch(i, visited, order, -1, nullptr);
     }
   }
+
+  // Переворачиваем порядок обхода
   reverse(order.begin(), order.end());
+
+  // Транспонированный граф
   Graph transposed = graph.Transpose();
+
   visited.assign(n + 1, false);
   component.assign(n + 1, 0);
   int comp_id = 1;
+
+  // Выполняем DFS на транспонированном графе для получения компонент
   for (int v : order) {
     if (!visited[v]) {
       vector<int> temp;
-      transposed.DFS(v, visited, temp, comp_id, &component);
+      transposed.DepthFirstSearch(v, visited, temp, comp_id, &component);
       ++comp_id;
     }
   }
+
   return comp_id - 1;
 }
 
@@ -82,6 +95,8 @@ int main() {
   int n;
   int m;
   cin >> n >> m;
+
+  // Создание графа
   Graph graph(n);
   for (int i = 0; i < m; ++i) {
     int from;
@@ -89,11 +104,18 @@ int main() {
     cin >> from >> to;
     graph.AddEdge(from, to);
   }
+
+  // Рассчёт компонент сильной связности
   vector<int> component;
   int total = CalculateStrongConnectedComponents(graph, component);
+
+  // Вывод результата
   cout << total << endl;
   for (int i = 1; i <= n; ++i) {
     cout << component[i] << ' ';
   }
   cout << endl;
+
+  return 0;
 }
+
